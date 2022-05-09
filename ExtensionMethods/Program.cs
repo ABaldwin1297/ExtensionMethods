1c89f6be03364ef7af305148e0d8991a55aee648
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-09 11:53:45 -0400

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods;
+
+var abc = "ABC";
+
+abc.Debug(); // instead of doing console.writeline, we createed a method on the other page as a quick way to do this
+
+var str = "abcdefg";
+
+str.Ucase();
+
+var i = 5;
+
+5.Fn1().ToString().Debug(); // Fn1 was a math method, need to use ToString to convert int to string and then use .Debug() method to write out the answer
